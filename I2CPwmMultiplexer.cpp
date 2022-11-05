bd68fbe1277516b33fde5455f9a3bf4aa16038d9
Sergii Lisovenko <sergeylcpp@gmail.com>
2022-11-05 20:45:57 +0300

    Fic

--- I2CPwmMultiplexer.cpp
+++ I2CPwmMultiplexer.cpp
@@ -1,231 +1,105 @@
 #include "I2CPwmMultiplexer.h"
+
+#include <cmath>
 #include <cstddef>
-#include <iostream>
-#include <thread>
+#include <unistd.h>
 
 #include "I2cBus.h"
 
+namespace
+{
+// Registers/etc:
+constexpr uint8_t MODE1 = 0x00;
+constexpr uint8_t MODE2 = 0x01;
+constexpr uint8_t SUBADR1 = 0x02;
+constexpr uint8_t SUBADR2 = 0x03;
+constexpr uint8_t SUBADR3 = 0x04;
+constexpr uint8_t PRESCALE = 0xFE;
+constexpr uint8_t LED0_ON_L = 0x06;
+constexpr uint8_t LED0_ON_H = 0x07;
+constexpr uint8_t LED0_OFF_L = 0x08;
+constexpr uint8_t LED0_OFF_H = 0x09;
+constexpr uint8_t ALL_LED_ON_L = 0xFA;
+constexpr uint8_t ALL_LED_ON_H = 0xFB;
+constexpr uint8_t ALL_LED_OFF_L = 0xFC;
+constexpr uint8_t ALL_LED_OFF_H = 0xFD;
 
-namespace {
-// REGISTER ADDRESSES
-#define PCA9685_MODE1 0x00      /**< Mode Register 1 */
-#define PCA9685_MODE2 0x01      /**< Mode Register 2 */
-#define PCA9685_SUBADR1 0x02    /**< I2C-bus subaddress 1 */
-#define PCA9685_SUBADR2 0x03    /**< I2C-bus subaddress 2 */
-#define PCA9685_SUBADR3 0x04    /**< I2C-bus subaddress 3 */
-#define PCA9685_ALLCALLADR 0x05 /**< LED All Call I2C-bus address */
-#define PCA9685_LED0_ON_L 0x06  /**< LED0 on tick, low byte*/
-#define PCA9685_LED0_ON_H 0x07  /**< LED0 on tick, high byte*/
-#define PCA9685_LED0_OFF_L 0x08 /**< LED0 off tick, low byte */
-#define PCA9685_LED0_OFF_H 0x09 /**< LED0 off tick, high byte */
-// etc all 16:  LED15_OFF_H 0x45
-#define PCA9685_ALLLED_ON_L 0xFA  /**< load all the LEDn_ON registers, low */
-#define PCA9685_ALLLED_ON_H 0xFB  /**< load all the LEDn_ON registers, high */
-#define PCA9685_ALLLED_OFF_L 0xFC /**< load all the LEDn_OFF registers, low */
-#define PCA9685_ALLLED_OFF_H 0xFD /**< load all the LEDn_OFF registers,high */
-#define PCA9685_PRESCALE 0xFE     /**< Prescaler for PWM output frequency */
-#define PCA9685_TESTMODE 0xFF     /**< defines the test mode to be entered */
+// Bits:
+constexpr uint8_t RESTART = 0x80;
+constexpr uint8_t SLEEP = 0x10;
+constexpr uint8_t ALLCALL = 0x01;
+constexpr uint8_t INVRT = 0x10;
+constexpr uint8_t OUTDRV = 0x04;
 
-// MODE1 bits
-#define MODE1_ALLCAL 0x01  /**< respond to LED All Call I2C-bus address */
-#define MODE1_SUB3 0x02    /**< respond to I2C-bus subaddress 3 */
-#define MODE1_SUB2 0x04    /**< respond to I2C-bus subaddress 2 */
-#define MODE1_SUB1 0x08    /**< respond to I2C-bus subaddress 1 */
-#define MODE1_SLEEP 0x10   /**< Low power mode. Oscillator off */
-#define MODE1_AI 0x20      /**< Auto-Increment enabled */
-#define MODE1_EXTCLK 0x40  /**< Use EXTCLK pin clock */
-#define MODE1_RESTART 0x80 /**< Restart enabled */
-// MODE2 bits
-#define MODE2_OUTNE_0 0x01 /**< Active LOW output enable input */
-#define MODE2_OUTNE_1 0x02 /**< Active LOW output enable input - high impedience */
-#define MODE2_OUTDRV 0x04  /**< totem pole structure vs open-drain */
-#define MODE2_OCH 0x08     /**< Outputs change on ACK vs STOP */
-#define MODE2_INVRT 0x10   /**< Output logic state inverted */
+}// namespace
 
-#define PCA9685_I2C_ADDRESS 0x40      /**< Default PCA9685 I2C Slave Address */
-#define FREQUENCY_OSCILLATOR 25000000 /**< Int. osc. frequency in datasheet */
-
-#define PCA9685_PRESCALE_MIN 3   /**< minimum prescale value */
-#define PCA9685_PRESCALE_MAX 255 /**< maximum prescale value */
-
-} // namespace
-
-I2CPwmMultiplexer::I2CPwmMultiplexer() {
+I2CPwmMultiplexer::I2CPwmMultiplexer()
+{
     _bus = std::make_unique<I2CBus>(1, 0x40);
+
+    setAllPwm(0, 0);
+    std::ignore = _bus->WriteByte(MODE2, (std::byte) OUTDRV);
+    std::ignore = _bus->WriteByte(MODE1, (std::byte) ALLCALL);
+    usleep(5'000);
+    std::byte data{0};
+    std::ignore = _bus->ReadByte(MODE1, &data);
+    auto mode1Val = std::to_integer<int>(data);
+    mode1Val &= ~SLEEP;
+    std::ignore = _bus->WriteByte(MODE1, static_cast<std::byte>(mode1Val));
+    usleep(5'000);
 }
 
 I2CPwmMultiplexer::~I2CPwmMultiplexer() = default;
 
-bool I2CPwmMultiplexer::isInit() const {
+bool I2CPwmMultiplexer::isInit() const
+{
     return _bus->IsOpen();
 }
 
-void I2CPwmMultiplexer::begin(uint8_t prescale) {
-    if (prescale) {
-        setExtClk(prescale);
-    } else {
-        // set a default frequency
-        setPWMFreq(1000);
-    }
-    // set the default internal frequency
-    setOscillatorFrequency(FREQUENCY_OSCILLATOR);
-}
+void I2CPwmMultiplexer::setPwmFreq(const double freqHz)
+{
+    _frequency = freqHz;
 
-void I2CPwmMultiplexer::reset() {
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(MODE1_RESTART));
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-}
+    auto prescaleval = 2.5e7;//    # 25MHz
+    prescaleval /= 4096.0;   //       # 12-bit
+    prescaleval /= freqHz;
+    prescaleval -= 1.0;
 
-void I2CPwmMultiplexer::sleep() {
-    std::byte data{0};
-    std::ignore = _bus->ReadByte(PCA9685_MODE1, &data);
-    auto awake = std::to_integer<uint8_t>(data);
-    uint8_t sleep = awake | MODE1_SLEEP; // set sleep bit high
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(sleep));
-    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-}
-
-void I2CPwmMultiplexer::wakeup() {
-    std::byte data{0};
-    std::ignore = _bus->ReadByte(PCA9685_MODE1, &data);
-    auto sleep = std::to_integer<uint8_t>(data);
-    uint8_t wakeup = (uint8_t) (sleep & ~MODE1_SLEEP); // set sleep bit low
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(wakeup));
-}
-
-void I2CPwmMultiplexer::setExtClk(uint8_t prescale) {
-    std::byte data{0};
-    std::ignore = _bus->ReadByte(PCA9685_MODE1, &data);
-    auto oldmode = std::to_integer<uint8_t>(data);
-    uint8_t newmode = (uint8_t) (oldmode & ~MODE1_RESTART) | MODE1_SLEEP; // sleep
-    std::ignore = _bus->WriteByte(PCA9685_MODE1,
-                                  static_cast<std::byte>(
-                                      newmode)); // go to sleep, turn off internal oscillator
-
-    // This sets both the SLEEP and EXTCLK bits of the MODE1 register to switch to
-    // use the external clock.
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(newmode |= MODE1_EXTCLK));
-
-    std::ignore = _bus->WriteByte(PCA9685_PRESCALE, static_cast<std::byte>(prescale)); // set the prescaler
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-    // clear the SLEEP bit to start
-    std::ignore = _bus->WriteByte(PCA9685_MODE1,
-                                  static_cast<std::byte>((newmode & ~MODE1_SLEEP) | MODE1_RESTART | MODE1_AI));
-}
-
-void I2CPwmMultiplexer::setPWMFreq(float freq) {
-    // Range output modulation frequency is dependant on oscillator
-    if (freq < 1)
-        freq = 1;
-    if (freq > 3500)
-        freq = 3500; // Datasheet limit is 3052=50MHz/(4*4096)
-
-    float prescaleval = static_cast<float>(((_oscillator_freq / (freq * 4096.0)) + 0.5) - 1);
-    std::cout << "prescale: " << prescaleval << "\n";
-    if (prescaleval < PCA9685_PRESCALE_MIN)
-        prescaleval = PCA9685_PRESCALE_MIN;
-    if (prescaleval > PCA9685_PRESCALE_MAX)
-        prescaleval = PCA9685_PRESCALE_MAX;
-    uint8_t prescale = (uint8_t) prescaleval;
+    auto prescale = static_cast<int>(std::round(prescaleval));
 
     std::byte data{0};
-    auto countRead = _bus->ReadByte(PCA9685_MODE1, &data);
-    auto oldmode = (uint8_t)(data);
+    std::ignore = _bus->ReadByte(MODE1, &data);
+    const auto oldMode = std::to_integer<int>(data);
+    auto newMode = (oldMode & 0x7F) | SLEEP;
 
-    uint8_t newmode = (uint8_t) ((oldmode & ~MODE1_RESTART) | MODE1_SLEEP);            // sleep
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(newmode));     // go to sleep
-    std::ignore = _bus->WriteByte(PCA9685_PRESCALE, static_cast<std::byte>(prescale)); // set the prescaler
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(oldmode));
-    std::this_thread::sleep_for(std::chrono::milliseconds(5));
-    // This sets the MODE1 register to turn on auto increment.
-    std::ignore = _bus->WriteByte(PCA9685_MODE1, static_cast<std::byte>(oldmode | MODE1_RESTART | MODE1_AI));
+    std::ignore = _bus->WriteByte(MODE1, static_cast<std::byte>(newMode));
+    std::ignore = _bus->WriteByte(PRESCALE, static_cast<std::byte>(prescale));
+    std::ignore = _bus->WriteByte(MODE1, (std::byte) oldMode);
+    usleep(5'000);
+    std::ignore = _bus->WriteByte(MODE1, static_cast<std::byte>(oldMode | RESTART));
 }
 
-void I2CPwmMultiplexer::setOutputMode(bool totempole) {
-    std::byte data{0};
-    std::ignore = _bus->ReadByte(PCA9685_MODE2, &data);
-    auto oldmode = std::to_integer<uint8_t>(data);
-    uint8_t newmode;
-    if (totempole) {
-        newmode = (uint8_t) (oldmode | MODE2_OUTDRV);
-    } else {
-        newmode = (uint8_t) (oldmode & ~MODE2_OUTDRV);
-    }
-    std::ignore = _bus->WriteByte(PCA9685_MODE2, static_cast<std::byte>(newmode));
+void I2CPwmMultiplexer::setPwm(const int channel, const uint16_t on, const uint16_t off)
+{
+    const auto channel_offset = 4 * channel;
+    std::ignore = _bus->WriteByte(LED0_ON_L + channel_offset, static_cast<std::byte>(on & 0xFF));
+    std::ignore = _bus->WriteByte(LED0_ON_H + channel_offset, static_cast<std::byte>(on >> 8));
+    std::ignore = _bus->WriteByte(LED0_OFF_L + channel_offset, static_cast<std::byte>(off & 0xFF));
+    std::ignore = _bus->WriteByte(LED0_OFF_H + channel_offset, static_cast<std::byte>(off >> 8));
 }
 
-uint8_t I2CPwmMultiplexer::getPWM(uint8_t num) {
-    uint16_t data;
-    std::ignore = _bus->ReadWord((uint8_t) (PCA9685_LED0_ON_L + 4 * num), data);
-    return data;
+void I2CPwmMultiplexer::setAllPwm(const uint16_t on, const uint16_t off)
+{
+    std::ignore = _bus->WriteByte(ALL_LED_ON_L, static_cast<std::byte>(on & 0xFF));
+    std::ignore = _bus->WriteByte(ALL_LED_ON_H, static_cast<std::byte>(on >> 8));
+    std::ignore = _bus->WriteByte(ALL_LED_OFF_L, static_cast<std::byte>(off & 0xFF));
+    std::ignore = _bus->WriteByte(ALL_LED_OFF_H, static_cast<std::byte>(off >> 8));
 }
 
-uint8_t I2CPwmMultiplexer::setPWM(uint8_t num, uint16_t on, uint16_t off) {
-    auto addr = (uint8_t) (PCA9685_LED0_ON_L + 4 * num);
-    std::byte data[4] = {static_cast<std::byte>(on),
-                         static_cast<std::byte>(on >> 8),
-                         static_cast<std::byte>(off),
-                         static_cast<std::byte>(off >> 8)};
-    std::ignore = _bus->WriteBytes(addr, 4, data);
-    return 1;
-}
-
-void I2CPwmMultiplexer::setPin(uint8_t num, uint16_t val, bool invert) {
-    // Clamp value between 0 and 4095 inclusive.
-    val = std::min(val, (uint16_t) 4095);
-    if (invert) {
-        if (val == 0) {
-            // Special value for signal fully on.
-            setPWM(num, 4096, 0);
-        } else if (val == 4095) {
-            // Special value for signal fully off.
-            setPWM(num, 0, 4096);
-        } else {
-            setPWM(num, 0, 4095 - val);
-        }
-    } else {
-        if (val == 4095) {
-            // Special value for signal fully on.
-            setPWM(num, 4096, 0);
-        } else if (val == 0) {
-            // Special value for signal fully off.
-            setPWM(num, 0, 4096);
-        } else {
-            setPWM(num, 0, val);
-        }
-    }
-}
-
-uint8_t I2CPwmMultiplexer::readPrescale(void) {
-    std::byte data{0};
-    std::ignore = _bus->ReadByte(PCA9685_PRESCALE, &data);
-    return std::to_integer<uint8_t>(data);
-}
-
-void I2CPwmMultiplexer::writeMicroseconds(uint8_t num, uint16_t Microseconds) {
-    double pulse = Microseconds;
-    double pulselength;
-    pulselength = 1000000; // 1,000,000 us per second
-
-    // Read prescale
-    uint16_t prescale = readPrescale();
-
-    // Calculate the pulse for PWM based on Equation 1 from the datasheet section
-    // 7.3.5
-    prescale += 1;
-    pulselength *= prescale;
-    pulselength /= _oscillator_freq;
-
-    pulse /= pulselength;
-
-    setPWM(num, 0, static_cast<uint16_t>(pulse));
-}
-
-void I2CPwmMultiplexer::setOscillatorFrequency(uint32_t freq) {
-    _oscillator_freq = freq;
-}
-
-uint32_t I2CPwmMultiplexer::getOscillatorFrequency() {
-    return _oscillator_freq;
+void I2CPwmMultiplexer::setPwmMs(const int channel, const double ms)
+{
+    auto period_ms = 1000.0 / _frequency;
+    auto bits_per_ms = 4096 / period_ms;
+    auto bits = ms * bits_per_ms;
+    setPwm(channel, 0, bits);
 }
--- main.cpp
+++ main.cpp
@@ -1,54 +1,25 @@
 #include "I2CPwmMultiplexer.h"
 #include <iostream>
-#include <thread>
-
-#define SERVOMIN 100 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX 2200// This is the 'maximum' pulse length count (out of 4096)
-#define SERVO_FREQ 60// Analog servos run at ~60 Hz updates
-#define USMIN 600    // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX 2400   // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-
-auto &pwm = I2CPwmMultiplexer::instance();
+#include <unistd.h>
 
 int main()
 {
+    auto &pwm = I2CPwmMultiplexer::instance();
     if (!pwm.isInit()) {
         std::cerr << "I2C not inited!\n";
         return 1;
     }
-    pwm.begin();
-    pwm.setPWMFreq(SERVO_FREQ);// Analog servos run at ~50 Hz updates
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    pwm.setPwmFreq(60);
 
-    uint8_t servonum = 0;
     while (true) {
-        for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-            pwm.setPWM(servonum, 0, pulselen);
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-            pwm.setPWM(servonum, 0, pulselen);
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-        // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
-        // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior.
-        for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
-            pwm.writeMicroseconds(servonum, microsec);
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-            pwm.writeMicroseconds(servonum, microsec);
-        }
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-        std::cout << "pwm for servo" << servonum << ": " << pwm.getPWM(servonum);
-        servonum++;
-        if (servonum > 7) servonum = 0;// Testing the first 8 servo channels
+        pwm.setPwm(0, 0, 370);
+        usleep(1'000'000);
+        pwm.setPwm(0, 0, 415);
+        usleep(1'000'000);
+        pwm.setPwm(0, 0, 460);
+        usleep(1'000'000);
+        pwm.setPwm(0, 0, 415);
+        usleep(1'000'000);
     }
     return 0;
 }
