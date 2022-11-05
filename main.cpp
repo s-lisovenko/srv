cd217d5337ce4edd57d968cda84b8c869b7c1628
Sergii Lisovenko <sergeylcpp@gmail.com>
2022-11-05 15:14:52 +0300

    fix2

--- main.cpp
+++ main.cpp
@@ -19,11 +19,16 @@ int main()
     pwm.begin();
     pwm.setOscillatorFrequency(27000000);
     pwm.setPWMFreq(2000);
-    bool lock = false;
-    for (uint16_t i = 0; i < 10; i++) {
-        pwm.setPWM(0, 0, lock ? 1000 : 2000);
-        lock = !lock;
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+    //    bool lock = false;
+    //    for (uint16_t i = 0; i < 10; i++) {
+    //        pwm.setPWM(0, 0, lock ? 1000 : 2000);
+    //        lock = !lock;
+    //        std::this_thread::sleep_for(std::chrono::seconds(2));
+    //    }
+    for (uint16_t i = 0; i < 4096; i += 8) {
+        for (uint8_t pwmnum = 0; pwmnum < 16; pwmnum++) {
+            pwm.setPWM(pwmnum, 0, (i + (4096 / 16) * pwmnum) % 4096);
+        }
     }
 
 
