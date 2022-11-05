357f318cb6e33b15b8fb143d49fdab05e2274dc6
Sergii Lisovenko <sergeylcpp@gmail.com>
2022-11-05 15:22:04 +0300

    fix 3

--- main.cpp
+++ main.cpp
@@ -18,19 +18,41 @@ int main()
 
     pwm.begin();
     pwm.setOscillatorFrequency(27000000);
-    pwm.setPWMFreq(2000);
-    //    bool lock = false;
-    //    for (uint16_t i = 0; i < 10; i++) {
-    //        pwm.setPWM(0, 0, lock ? 1000 : 2000);
-    //        lock = !lock;
-    //        std::this_thread::sleep_for(std::chrono::seconds(2));
-    //    }
-    for (uint16_t i = 0; i < 4096; i += 8) {
-        for (uint8_t pwmnum = 0; pwmnum < 16; pwmnum++) {
-            pwm.setPWM(pwmnum, 0, (i + (4096 / 16) * pwmnum) % 4096);
-        }
-    }
+    pwm.setPWMFreq(SERVO_FREQ);// Analog servos run at ~50 Hz updates
+    uint8_t servonum = 0;
 
+    for (int i = 0; i < 10; ++i) {
+        // Drive each servo one at a time using setPWM()
+
+        for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+            pwm.setPWM(servonum, 0, pulselen);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+            pwm.setPWM(servonum, 0, pulselen);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
+        // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior.
+        for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
+            pwm.writeMicroseconds(servonum, microsec);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
+            pwm.writeMicroseconds(servonum, microsec);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+
+        servonum++;
+        if (servonum > 7) servonum = 0;// Testing the first 8 servo channels
+    }
 
     return 0;
 }
